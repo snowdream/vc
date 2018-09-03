54a5389f1bede62b5086650ba1104a1077b436db
snowdream <yanghui1986527@gmail.com>
2018-09-03 22:47:07 +0800

    Add: Vue

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js
 module.exports = {
     // 基本路径
-    baseUrl: '/',
+    baseUrl: './',
     // 输出文件目录
     outputDir: 'www',
     //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
